5b8de7f021ceadb6150d03f1d86b0c9dbdb93a53
Rafael Coelho Lopes de Sa <rclsa@pb-d-128-141-142-126.cern.ch>
2015-05-26 18:02:49 +0200

    command line option

--- bin/analysis.cc
+++ bin/analysis.cc
@@ -6,6 +6,7 @@
 #include "TBranch.h"
 #include "TFile.h"
 #include "TLorentzVector.h"
+#include "TH2D.h"
 
 #include <algorithm>
 #include <iostream> // needed for io
@@ -34,10 +35,17 @@ vector<TLorentzVector> findBS(vector<GenParticle_p5>& partList) {
 }
 
 int main(int argc, char** argv){
+
+  if (argc != 3) {
+    cout << "analysis.exe [eventFile] [outputFile]" << endl;
+    return 0;
+  }
+  
+  TH2D* nsubjets = new TH2D("nsubjets", "", 10, -0.5, 9.5, 10, -0.5, 9.5);
   
   // Gets the input and stores in fastjet
   std::vector<fastjet::PseudoJet> input_particles;
-  TFile* dataFile = TFile::Open("data/a20a20.root");
+  TFile* dataFile = TFile::Open(argv[1]);
   TTree* CollectionTree = (TTree*) dataFile->Get("CollectionTree");
   TBranch* branch = CollectionTree->GetBranch("McEventCollection_p5_GEN_EVENT");
   McEventCollection_p5* event = 0;
@@ -48,13 +56,14 @@ int main(int argc, char** argv){
   CollectionTree->SetBranchAddress("McEventCollection_p5_GEN_EVENT", &event);
 
   // Which jet we want
-  double R = 0.4;
-  fastjet::JetDefinition jet_def(fastjet::antikt_algorithm, R);
+  double R = 1.0;
+  fastjet::JetDefinition jet_def(fastjet::cambridge_algorithm, R);
 
   Long64_t nentries = CollectionTree->GetEntries();
   cout << "Reading " << nentries << " events" << endl;
   for (Long64_t ievt=0; ievt<nentries; ievt++) {
-
+    if (ievt % 500 == 0) cout << "Event  " << ievt << endl;
+    
     branch->GetEntry(ievt);
     TLorentzVector partvec;
     input_particles.clear();
@@ -92,57 +101,34 @@ int main(int argc, char** argv){
     // cluster the jets
     fastjet::ClusterSequence clust_seq(input_particles, jet_def);
 
-
-    double ptmin = 10000.0;
+    // and look for subjets
+    double Rsub = 0.5;
+    double dcut = pow(Rsub/R,2);
+    double ptmin = 20000.0;
     vector<fastjet::PseudoJet> inclusive_jets = sorted_by_pt(clust_seq.inclusive_jets(ptmin));
+
     vector<TLorentzVector> inclusive_jets_vec;
     for (auto jet : inclusive_jets) {
       TLorentzVector jetvec(jet.px(), jet.py(), jet.pz(), jet.e());
       inclusive_jets_vec.push_back(jetvec);
+      vector<fastjet::PseudoJet> subjets = sorted_by_pt(jet.exclusive_subjets(dcut));
+      nsubjets->Fill(inclusive_jets.size(),subjets.size());
     }
-
     vector<TLorentzVector> matched_jets;
-    for (auto bs : theBS) {
-      auto min = min_element(inclusive_jets_vec.begin(), inclusive_jets_vec.end(),
-			     [bs](TLorentzVector v1, TLorentzVector v2)->bool {return bs.DeltaR(v1) < bs.DeltaR(v2);});
-      //      if (bs.DeltaR(*min) < R)
-      matched_jets.push_back(*min);
-
+    if (inclusive_jets_vec.size() > 0) {
+      for (auto bs : theBS) {
+	auto min = min_element(inclusive_jets_vec.begin(), inclusive_jets_vec.end(),
+			       [bs](TLorentzVector v1, TLorentzVector v2)->bool {return bs.DeltaR(v1) < bs.DeltaR(v2);});
+	matched_jets.push_back(*min);
+      }
     }
 
-    // Look at the first a->bb
-    cout << "First decay a->bb" << endl;
-    cout << "   b parton 1: " << theBS[0].Eta() << " " << theBS[0].Phi() << " " << theBS[0].Pt()/1000. << endl;
-    cout << "   b parton 2: " << theBS[1].Eta() << " " << theBS[1].Phi() << " " << theBS[1].Pt()/1000. << endl;
-    cout << "   a mass    : " << (theBS[0] + theBS[1]).M()/1000. << endl;
-    cout << "   bb dR     : " << theBS[0].DeltaR(theBS[1]) << endl;
-    if (matched_jets[0] == matched_jets[1]) {
-      cout << "   Merged case! " << endl;
-      cout << "     Fat jet     : " << matched_jets[0].Eta() << " " << matched_jets[0].Phi() << " " << matched_jets[0].Pt()/1000. << endl;
-      cout << "     Jet mass    : " << matched_jets[0].M()/1000. << endl;
-    } else {
-      cout << "   Resolved case! " << endl;
-      cout << "     jet 1       : " << matched_jets[0].Eta() << " " << matched_jets[0].Phi() << " " << matched_jets[0].Pt()/1000. << endl;
-      cout << "     jet 2       : " << matched_jets[1].Eta() << " " << matched_jets[1].Phi() << " " << matched_jets[1].Pt()/1000. << endl;
-      cout << "     dijet mass  : " << (matched_jets[0]+matched_jets[1]).M()/1000. << endl;
-    }
-    cout << "Second decay a->bb" << endl;
-    cout << "   b parton 1: " << theBS[2].Eta() << " " << theBS[2].Phi() << " " << theBS[2].Pt()/1000. << endl;
-    cout << "   b parton 2: " << theBS[3].Eta() << " " << theBS[3].Phi() << " " << theBS[3].Pt()/1000. << endl;
-    cout << "   a mass    : " << (theBS[2] + theBS[3]).M()/1000. << endl;
-    cout << "   bb dR     : " << theBS[2].DeltaR(theBS[3]) << endl;
-    if (matched_jets[2] == matched_jets[3]) {
-      cout << "   Merged case! " << endl;
-      cout << "     Fat jet     : " << matched_jets[2].Eta() << " " << matched_jets[2].Phi() << " " << matched_jets[2].Pt()/1000. << endl;
-      cout << "     Jet mass    : " << matched_jets[2].M()/1000. << endl;
-    } else {
-      cout << "   Resolved case! " << endl;
-      cout << "     jet 1       : " << matched_jets[2].Eta() << " " << matched_jets[2].Phi() << " " << matched_jets[2].Pt()/1000. << endl;
-      cout << "     jet 2       : " << matched_jets[3].Eta() << " " << matched_jets[3].Phi() << " " << matched_jets[3].Pt()/1000. << endl;
-      cout << "     dijet mass  : " << (matched_jets[2]+matched_jets[3]).M()/1000. << endl;
-    }
       
   }
+  dataFile->Close();
+  TFile* resultFile = TFile::Open(argv[2], "RECREATE");
+  nsubjets->Write();
+  resultFile->Close();
   return 0;
 
 }
